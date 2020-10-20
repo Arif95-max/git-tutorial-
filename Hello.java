5b2409d88b2179c2e7776bb08d115ce0998c56c5
Arifur Rahman <arifcse95@gmail.com>
2020-10-21 02:54:56 +0600

    Added a new method called greting to Hello.java

--- Hello.java
+++ Hello.java
@@ -2,7 +2,13 @@ public class Hello {
     public static void main(String[]args){
 
         System.out.println("vs code is Awesome");
+        greting("Arifur Rahman");
 
+    }
+    public static void greting(String name){
+
+
+        System.out.println("Welcome" + name);
     }
 
 
